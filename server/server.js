6f751032f64c53e9c6c7419ae916c95a2adc15b9
David Spector <davidspector9@gmail.com>
2023-04-22 23:29:11 -0700

    Complete CRUD implementation of generic events

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+const { 
+  v4: uuidv4,
+} = require('uuid');
+
 const express = require("express");
 const app = express();
 const mapFunc = require('./maps/maps.js');
@@ -18,251 +22,251 @@ app.use(express.urlencoded({extended: true}));
 
 // helper functions
 
-const getAllUsers = async () => {
-  const usersRef = db.collection("users");
-  const response = await usersRef.get();
-  let responseArr = [];
-  response.forEach(doc => {
-    responseArr.push(doc.data());
-  });
-  return responseArr;
-}
+// const getAllUsers = async () => {
+//   const usersRef = db.collection("users");
+//   const response = await usersRef.get();
+//   let responseArr = [];
+//   response.forEach(doc => {
+//     responseArr.push(doc.data());
+//   });
+//   return responseArr;
+// }
 
-const getUserInfo = async (username) => {
-  const userRef = db.collection("users").doc(username);
-  const user = await userRef.get();
-  if (typeof user.data() === 'undefined') {
-    return 0;
-  }
-  return user.data();
-}
+// const getUserInfo = async (username) => {
+//   const userRef = db.collection("users").doc(username);
+//   const user = await userRef.get();
+//   if (typeof user.data() === 'undefined') {
+//     return 0;
+//   }
+//   return user.data();
+// }
 
-const checkUserExists = async (username) => {
-  const users = await getAllUsers();
-  let user = undefined;
-  users.forEach(curUser => {
-    if (curUser.username === username){
-      user = curUser;
-    }
-  })
-  if (typeof user === 'undefined') {
-    return 0;
-  }
-  return 1;
-}
+// const checkUserExists = async (username) => {
+//   const users = await getAllUsers();
+//   let user = undefined;
+//   users.forEach(curUser => {
+//     if (curUser.username === username){
+//       user = curUser;
+//     }
+//   })
+//   if (typeof user === 'undefined') {
+//     return 0;
+//   }
+//   return 1;
+// }
 
-// returns 0 if not, and user list if true
-const isUserOnDepartingFlight = async (flightNumber, username) => {
-  const flightRef = db.collection("departures").doc(flightNumber);
-    const flight = (await flightRef.get()).data();
-    let userArr = [];
-    if (flight !== undefined) {
-      userArr = flight.users;
-    }
-    userArr.forEach(user => {
-      if (user === username){
-        userArr = 0;
-      }
-    })
-    return userArr;
-}
+// // returns 0 if not, and user list if true
+// const isUserOnDepartingFlight = async (flightNumber, username) => {
+//   const flightRef = db.collection("departures").doc(flightNumber);
+//     const flight = (await flightRef.get()).data();
+//     let userArr = [];
+//     if (flight !== undefined) {
+//       userArr = flight.users;
+//     }
+//     userArr.forEach(user => {
+//       if (user === username){
+//         userArr = 0;
+//       }
+//     })
+//     return userArr;
+// }
 
-const isUserOnArrivingFlight = async (flightNumber, username) => {
-  const flightRef = db.collection("arrivals").doc(flightNumber);
-    const flight = (await flightRef.get()).data();
-    let userArr = [];
-    if (flight !== undefined) {
-      userArr = flight.users;
-    }
-    userArr.forEach(user => {
-      if (user === username){
-        userArr = 0;
-      }
-    })
-    return userArr;
-}
+// const isUserOnArrivingFlight = async (flightNumber, username) => {
+//   const flightRef = db.collection("arrivals").doc(flightNumber);
+//     const flight = (await flightRef.get()).data();
+//     let userArr = [];
+//     if (flight !== undefined) {
+//       userArr = flight.users;
+//     }
+//     userArr.forEach(user => {
+//       if (user === username){
+//         userArr = 0;
+//       }
+//     })
+//     return userArr;
+// }
 
-const isDepartingFlightInUser = async (flightNumber, username) => {
-  let flightArr = [];
-  const user = await getUserInfo(username);
-  if ( user.departures !== undefined) {
-    flightArr = user.departures;
-  }
-  flightArr.forEach(flightNum => {
-    if (flightNum === flightNumber){
-      flightArr = 0;
-    }
-  })
-  return flightArr;
-}
+// const isDepartingFlightInUser = async (flightNumber, username) => {
+//   let flightArr = [];
+//   const user = await getUserInfo(username);
+//   if ( user.departures !== undefined) {
+//     flightArr = user.departures;
+//   }
+//   flightArr.forEach(flightNum => {
+//     if (flightNum === flightNumber){
+//       flightArr = 0;
+//     }
+//   })
+//   return flightArr;
+// }
 
-const isArrivingFlightInUser = async (flightNumber, username) => {
-  let flightArr = [];
-  const user = await getUserInfo(username);
-  if ( user.arrivals !== undefined) {
-    flightArr = user.departures;
-  }
-  flightArr.forEach(flightNum => {
-    if (flightNum === flightNumber){
-      flightArr = 0;
-    }
-  })
-  return flightArr;
-}
+// const isArrivingFlightInUser = async (flightNumber, username) => {
+//   let flightArr = [];
+//   const user = await getUserInfo(username);
+//   if ( user.arrivals !== undefined) {
+//     flightArr = user.departures;
+//   }
+//   flightArr.forEach(flightNum => {
+//     if (flightNum === flightNumber){
+//       flightArr = 0;
+//     }
+//   })
+//   return flightArr;
+// }
 
-// routes
+// // routes
 
-app.post('/create/user', async (req, res) => {
-  try {
-    const id = req.body.username;
-    const usersRef = db.collection("users");
-    const users = await usersRef.get();
-    users.forEach(doc => {
-      if (doc.data().username === id){
-        throw new Error("username is taken");
-      }      
-    });
-    const userJson = {
-      username: req.body.username,
-      bio: req.body.bio,
-      socialMedia: req.body.socialMedia,
-      uclaHome: req.body.uclaHome,
-    };
-    const response = db.collection("users").doc(id).set(userJson);
-    res.send(response);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-})
+// app.post('/create/user', async (req, res) => {
+//   try {
+//     const id = req.body.username;
+//     const usersRef = db.collection("users");
+//     const users = await usersRef.get();
+//     users.forEach(doc => {
+//       if (doc.data().username === id){
+//         throw new Error("username is taken");
+//       }      
+//     });
+//     const userJson = {
+//       username: req.body.username,
+//       bio: req.body.bio,
+//       socialMedia: req.body.socialMedia,
+//       uclaHome: req.body.uclaHome,
+//     };
+//     const response = db.collection("users").doc(id).set(userJson);
+//     res.send(response);
+//   } catch (error) {
+//     console.log(error);
+//     res.send(error);
+//   }
+// })
 
-app.post('/create/:username/addDepartureFlight', async (req, res) => {
-  try {
-    const id = req.body.flightNumber;
-    const username = req.params.username;
+// app.post('/create/:username/addDepartureFlight', async (req, res) => {
+//   try {
+//     const id = req.body.flightNumber;
+//     const username = req.params.username;
 
-    // make sure user exists
-    const userExists = await checkUserExists(username);
-    if (userExists === 0) {
-      throw new Error("User does not exist");
-    }
+//     // make sure user exists
+//     const userExists = await checkUserExists(username);
+//     if (userExists === 0) {
+//       throw new Error("User does not exist");
+//     }
 
-    // make sure user can be added to flight
-    let usersOnFlight = await isUserOnDepartingFlight(id, username);
-    if (usersOnFlight === 0) {
-      throw new Error("User already on this flight");
-    }
-    usersOnFlight.push(username);
-
-
-    // check user's departing flights
-    let userDepartingFlights = await isDepartingFlightInUser(id, username);
-    if (userDepartingFlights === 0) {
-      throw new Error("Arrival already recorded for current user");
-    }
-    userDepartingFlights.push(id);
-
-    // update user's departing flights
-    const userRef = db.collection("users").doc(username);
-    userRef.update({departures: userDepartingFlights});
-
-    // update departing flight
-    const departureJson = {
-      flightNumber: req.body.flightNumber,
-      takeOffTime: req.body.takeOffTime,
-      terminal: req.body.terminal,
-      users: usersOnFlight
-    }
-    const response = db.collection("departures").doc(id).set(departureJson);
-
-    res.send(response);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-})
-
-app.post('/create/:username/addArrivalFlight', async (req, res) => {
-  try {
-    const id = req.body.flightNumber;
-    const username = req.params.username;
-
-    // make sure user exists
-    const userExists = await checkUserExists(username);
-    if (userExists === 0) {
-      throw new Error("User does not exist");
-    }
-
-    // make sure user can be added to flight
-    let usersOnFlight = await isUserOnArrivingFlight(id, username);
-    if (usersOnFlight === 0) {
-      throw new Error("User already on this flight");
-    }
-    usersOnFlight.push(username);
+//     // make sure user can be added to flight
+//     let usersOnFlight = await isUserOnDepartingFlight(id, username);
+//     if (usersOnFlight === 0) {
+//       throw new Error("User already on this flight");
+//     }
+//     usersOnFlight.push(username);
 
 
-    // check user's arriving flights
-    let userArrivingFlights = await isArrivingFlightInUser(id, username);
-    if (userArrivingFlights === 0) {
-      throw new Error("Arrival already recorded for current user");
-    }
-    userArrivingFlights.push(id);
+//     // check user's departing flights
+//     let userDepartingFlights = await isDepartingFlightInUser(id, username);
+//     if (userDepartingFlights === 0) {
+//       throw new Error("Arrival already recorded for current user");
+//     }
+//     userDepartingFlights.push(id);
 
-    // create arrival data type
-    const arrivalJson = {
-      flightNumber: req.body.flightNumber,
-      takeOffTime: req.body.takeOffTime,
-      terminal: req.body.terminal,
-      users: usersOnFlight
-    }
+//     // update user's departing flights
+//     const userRef = db.collection("users").doc(username);
+//     userRef.update({departures: userDepartingFlights});
 
-    // update user and flight arrays
-    const userRef = db.collection("users").doc(username);
-    userRef.update({arrivals: userArrivingFlights});
-    const response = db.collection("arrivals").doc(id).set(arrivalJson);
+//     // update departing flight
+//     const departureJson = {
+//       flightNumber: req.body.flightNumber,
+//       takeOffTime: req.body.takeOffTime,
+//       terminal: req.body.terminal,
+//       users: usersOnFlight
+//     }
+//     const response = db.collection("departures").doc(id).set(departureJson);
 
-    res.send(response);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-})
+//     res.send(response);
+//   } catch (error) {
+//     console.log(error);
+//     res.send(error);
+//   }
+// })
 
-app.get('/read/users', async (req, res) => {
-  try {
-    const users = await getAllUsers();
-    res.send(users);
-  } catch(error) {
-    res.send(error);
-  }
-})
+// app.post('/create/:username/addArrivalFlight', async (req, res) => {
+//   try {
+//     const id = req.body.flightNumber;
+//     const username = req.params.username;
 
-app.get('/read/user/:username', async (req, res) => {
-  try {
-    const user = await getUserInfo(req.params.username);
-    if (user === 0) {
-      throw new Error("User does not exist");
-    }
-    res.send(user);
-  } catch(error) {
-    console.log(error);
-    res.send(error);
-  }
-})
+//     // make sure user exists
+//     const userExists = await checkUserExists(username);
+//     if (userExists === 0) {
+//       throw new Error("User does not exist");
+//     }
 
-app.post('/update', async (req, res) => {
-  try {
-    const id = req.body.id;
-    const newFlightTime = req.body.newFlightTime;
-    const userRef = await db.collection("users").doc(id)
-    .update({
-      flightTime: newFlightTime
-    })
-    res.send(userRef);
-  } catch(error) {
-    res.send(error);
-  }
-})
+//     // make sure user can be added to flight
+//     let usersOnFlight = await isUserOnArrivingFlight(id, username);
+//     if (usersOnFlight === 0) {
+//       throw new Error("User already on this flight");
+//     }
+//     usersOnFlight.push(username);
+
+
+//     // check user's arriving flights
+//     let userArrivingFlights = await isArrivingFlightInUser(id, username);
+//     if (userArrivingFlights === 0) {
+//       throw new Error("Arrival already recorded for current user");
+//     }
+//     userArrivingFlights.push(id);
+
+//     // create arrival data type
+//     const arrivalJson = {
+//       flightNumber: req.body.flightNumber,
+//       takeOffTime: req.body.takeOffTime,
+//       terminal: req.body.terminal,
+//       users: usersOnFlight
+//     }
+
+//     // update user and flight arrays
+//     const userRef = db.collection("users").doc(username);
+//     userRef.update({arrivals: userArrivingFlights});
+//     const response = db.collection("arrivals").doc(id).set(arrivalJson);
+
+//     res.send(response);
+//   } catch (error) {
+//     console.log(error);
+//     res.send(error);
+//   }
+// })
+
+// app.get('/read/users', async (req, res) => {
+//   try {
+//     const users = await getAllUsers();
+//     res.send(users);
+//   } catch(error) {
+//     res.send(error);
+//   }
+// })
+
+// app.get('/read/user/:username', async (req, res) => {
+//   try {
+//     const user = await getUserInfo(req.params.username);
+//     if (user === 0) {
+//       throw new Error("User does not exist");
+//     }
+//     res.send(user);
+//   } catch(error) {
+//     console.log(error);
+//     res.send(error);
+//   }
+// })
+
+// app.post('/update', async (req, res) => {
+//   try {
+//     const id = req.body.id;
+//     const newFlightTime = req.body.newFlightTime;
+//     const userRef = await db.collection("users").doc(id)
+//     .update({
+//       flightTime: newFlightTime
+//     })
+//     res.send(userRef);
+//   } catch(error) {
+//     res.send(error);
+//   }
+// })
 
 const getDriveTime = async() => {
   const t = await mapFunc.getRideTime();
@@ -282,26 +286,31 @@ const setDepartureTime = async (leaveDate) => {
   console.log("In server, set departure time to", leaveDate);
 }
 
-// SECOND ROUND OF ENDPOINTS
+// SECOND ROUND OF ENDPOINTS -> (CRUD)
 
+// Create Generic Event
 app.post('/create/event/timed', async (req, res) => {
   try {
     const username = req.body.username;
-    const flightTime = req.body.flightTime;
+    const flight_time = req.body.flight_time;
+    const arrival_or_departure = req.body.arrival_or_departure;
     const eventsRef = db.collection("events");
     const events = await eventsRef.get();
     events.forEach(doc => {
-      if (doc.data().username === username && doc.data().flight_time === flightTime) {
-        throw new Error("user and flight time combination is already being used");
+      if ((doc.data().username === username) && (doc.data().flight_time === flight_time) && (doc.data().arrival_or_departure === arrival_or_departure)) {
+        throw new Error("user, flight time, and flight direction combination is already being used");
       }
     })
+    
+    const uuid = uuidv4();
     const eventJson = {
       username: req.body.username,
       social_media_handle: req.body.social_media_handle,
       flight_time: req.body.flight_time,
       arrival_or_departure: req.body.arrival_or_departure,
+      uuid: uuid,
     };
-    const response = db.collection("events").add(eventJson);
+    const response = db.collection("events").doc(uuid).set(eventJson);
     res.send(response);    
   } catch (error) {
     console.log(error);
@@ -309,13 +318,13 @@ app.post('/create/event/timed', async (req, res) => {
   }
 })
 
+// Read generic event
 app.get('/read/timed/:id', async (req, res) => {
   try {
     const id = req.params.id;
     const eventRef = db.collection("events").doc(String(id));
     const event = await eventRef.get();
     let info; 
-    console.log(event.data());
     if (typeof (event.data()) === 'undefined') {
       throw new Error("event not found");
     }
@@ -329,6 +338,7 @@ app.get('/read/timed/:id', async (req, res) => {
   }
 })
 
+// Read all generic events
 app.get('/read/allTimed', async (req, res) => {
   try {
     const eventsRef = db.collection("events");
@@ -343,6 +353,45 @@ app.get('/read/allTimed', async (req, res) => {
     res.send(error);
   }
 })
+
+// Update generic event
+app.post('/update/timed/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const eventRef = db.collection("events").doc(String(id));
+    const event = await eventRef.get();
+    if (typeof (event.data()) === 'undefined') {
+      throw new Error("event not found");
+    }
+    const result = eventRef.update({
+      username: req.body.username,
+      social_media_handle: req.body.social_media_handle,
+      flight_time: req.body.flight_time,
+      arrival_or_departure: req.body.arrival_or_departure,
+    })
+    
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+}})
+
+// Delete generic event
+app.delete('/delete/timed/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const eventRef = db.collection("events").doc(String(id));
+    const event = await eventRef.get();
+    if (typeof (event.data()) === 'undefined') {
+      throw new Error("event not found");
+    }
+    const result = await db.collection("events").doc(String(id)).delete();
+    
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+}})
 
 //cors
 app.use(cors({
