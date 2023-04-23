05a4efc26437662d83d685fab49c1dbaf328b3a9
pinklz <116235230+pinklz@users.noreply.github.com>
2023-04-22 19:46:53 -0700

    Updated map functions

--- server/maps/maps.js
+++ server/maps/maps.js
@@ -5,15 +5,20 @@ function secondsSinceEpoch(date) {
     return Math.floor((date.getTime() - Date.UTC(1970, 0, 1)) / 1000);
 };
 
+//sets default time to now
 let user_date = new Date();
-console.log(user_date);
-//takes in a date object
+
+//takes in a date object, sets departure time to input if called
 function getDepartureTime(req, res) {
-    user_date = new Date(req.body.date);
+    user_date = req;
+    console.log(user_date);
 }
 
+// const testDate = new Date(2023, 3, 24, 10, 30, 0);
+// getDepartureTime(testDate);
+
 const seconds = secondsSinceEpoch(user_date);
-console.log(seconds);
+//console.log(seconds);
 
 const url_start = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=34.0692755%2C-118.44578&destinations=33.942791%2C-118.410042&departure_time=';
 const end_url = '&key=AIzaSyCf3Use0Z0Ff9lARNzlxcKzahBWVmpm4jY';
@@ -27,25 +32,50 @@ const url = {
 
 let urlString = JSON.stringify(url.full_url);
 urlString = urlString.replace(/['"]+/g, '');
-console.log(urlString);
+//console.log(urlString);
 
-var config = {
-    method:'get',
-    url: `${urlString}`,
-    headers: {}
+
+function fetchData() {
+    return axios.get(`${urlString}`)
+    .then(response => {
+        const dataString = JSON.stringify(response.data);
+        const mapsObj = JSON.parse(dataString);
+        return mapsObj;
+    })
+    .catch(error => {
+        console.error(error);
+    });
 };
 
-axios(config)
-.then(function(response) {
-    const dataString = JSON.stringify(response.data);
-    const mapsObj = JSON.parse(dataString);
-    const ride_time_traffic = mapsObj.rows[0].elements[0].duration_in_traffic.text;
-    const ride_distance = mapsObj.rows[0].elements[0].distance.text;
-    const ride_time_notraffic = mapsObj.rows[0].elements[0].duration.text;
-    console.log(ride_time_traffic);
-    console.log(ride_time_notraffic);
-    console.log(ride_distance);
-}) 
-.catch (function(error) {
-    console.log(error);
-});
+
+function getRideTime() {
+    fetchData()
+    .then(data => {
+        const ride_time_traffic = data.rows[0].elements[0].duration_in_traffic.text;
+        //console.log(ride_time_traffic);
+        return ride_time_traffic;
+    })
+    .catch(error => {
+        console.error(error);
+    })
+}
+
+function getRideDist() {
+    fetchData()
+    .then(data => {
+        const ride_distance = data.rows[0].elements[0].distance.text;
+        //console.log(ride_distance);
+        return ride_distance;
+    })
+    .catch(error => {
+        console.error(error);
+    })
+}
+
+
+module.exports = {
+    getRideTime,
+    getRideDist,
+    getDepartureTime
+};
+
